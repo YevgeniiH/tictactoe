493390c681fe52a7b9856ae425fe9ee8d405c64c
YevgeniiH <zhenya1gapon@gmail.com>
2014-10-22 18:38:25 +0300

    Добавлено главное меню

--- gen/ua/cn/defence1lab/tictactoe/R.java
+++ gen/ua/cn/defence1lab/tictactoe/R.java
@@ -29,16 +29,16 @@ public final class R {
         public static final int action_settings=0x7f080011;
         public static final int button1=0x7f080003;
         public static final int button2=0x7f080004;
-        public static final int button3=0x7f080005;
-        public static final int button4=0x7f080007;
-        public static final int button5=0x7f080006;
+        public static final int button3=0x7f080009;
+        public static final int button4=0x7f080006;
+        public static final int button5=0x7f080005;
         public static final int button6=0x7f08000b;
         public static final int button7=0x7f08000d;
         public static final int button8=0x7f08000e;
         public static final int button9=0x7f08000f;
-        public static final int main_l=0x7f080008;
+        public static final int main_l=0x7f080007;
         public static final int tableRow1=0x7f080002;
-        public static final int tableRow2=0x7f080009;
+        public static final int tableRow2=0x7f080008;
         public static final int tableRow3=0x7f08000a;
         public static final int tableRow4=0x7f08000c;
         public static final int tableRow5=0x7f080010;
@@ -52,9 +52,11 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int aboutS=0x7f050005;
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int rulesS=0x7f050004;
         public static final int title_activity_main=0x7f050003;
     }
     public static final class style {
--- src/ua/cn/defence1lab/tictactoe/MainActivity.java
+++ src/ua/cn/defence1lab/tictactoe/MainActivity.java
@@ -1,6 +1,8 @@
 package ua.cn.defence1lab.tictactoe;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -16,5 +18,55 @@ public class MainActivity extends Activity {
 	public void newGameB(View view) {
 		Intent intent = new Intent (this, GameActivity.class);
 		startActivity(intent);
+	}
+
+	@Override
+	public void onBackPressed() {
+		AlertDialog.Builder adb = new AlertDialog.Builder(this);
+		adb.setCancelable(false)
+        // заголовок
+        .setTitle("Выход")
+        // сообщение
+        .setMessage("Вы действительно хотите выйти")
+        // иконка
+        .setIcon(android.R.drawable.ic_dialog_alert)
+        // кнопка положительного ответа
+        .setPositiveButton("Да",
+        		new DialogInterface.OnClickListener()
+		        {
+		            public void onClick(DialogInterface dialog, int which) 
+		            {
+		                MainActivity.this.finish();    
+		            }
+		        })
+        // кнопка отрицательного ответа
+        .setNegativeButton("Нет", null)
+        .show();
+    }
+	
+	public void rulesB (View viwer) {
+		AlertDialog.Builder adb = new AlertDialog.Builder(this);
+        // заголовок
+        adb.setTitle("Правила")
+        // сообщение
+        .setMessage(R.string.rulesS)
+        // иконка
+        .setIcon(android.R.drawable.ic_dialog_info)
+        // кнопка отрицательного ответа
+        .setNeutralButton("OK", null)
+        .show();
+	}
+	
+	public void aboutB (View viwer) {
+		AlertDialog.Builder adb = new AlertDialog.Builder(this);
+        // заголовок
+        adb.setTitle("Про игру")
+        // сообщение
+        .setMessage(R.string.aboutS)
+        // иконка
+        .setIcon(android.R.drawable.ic_dialog_info)
+        // кнопка отрицательного ответа
+        .setNeutralButton("OK", null)
+        .show();
 	}
 }
